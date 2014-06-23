b6f53051296a2464096c04487780fedc6cefaadd
Peter Szymczak <pete@defdesign.net>
2014-06-22 20:37:38 -0700

    require.js

---
+++ contents/assets/js/app.js
@@ -0,0 +1,11 @@
+define(['domReady', 'cover', 'tweenlite', 'easing', 'easeljs'], function (domReady, cover, tweenlite, easing, easeljs) {
+  domReady(function () {
+
+    if ($('.frame').length){
+      cover.init( $('.frame') );
+    }
+
+
+
+  });
+});
