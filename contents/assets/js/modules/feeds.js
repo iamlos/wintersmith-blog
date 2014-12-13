62484dc68b1c99279ef0571e68504d81b1f6aced
Peter Szymczak <pete@defdesign.net>
2014-12-12 23:22:57 -0800

    ajax change

--- contents/assets/js/modules/feeds.js
+++ contents/assets/js/modules/feeds.js
@@ -1,68 +1,37 @@
 /**
- *   Feeds
- */
+*   Front Cover
+*/
 define([], function(){
   return {
 
     init: function(){
-      var self = this;
-      this.getFeeds();
-      this.setHeights();
 
-      $(window).resize(function(event) {
-        self.setHeights();
-      });
+      this.getFeeds();
 
     },
-
     getFeeds: function(){
-      //var self = this;
-      var url = 'http://23.239.7.50/api';
+      var url = 'http://owns.io/api';
       $.ajax({
-           url: url,
-           contentType: 'application/json; charset=utf-8',
-           data: {},
-           success: this.ajaxSuccess,
-
-           xhr: function(){
-             var xhr = new window.XMLHttpRequest();
-
-             //Download progress
-             xhr.addEventListener('progress', function(evt){
-
-
-               if (evt.lengthComputable) {
-                 var percentComplete = evt.loaded / evt.total;
-
-                 // Do something with download progress
-                 var statusbar = Math.ceil(percentComplete * 100);
-
-                 $('.loading').css('width', statusbar + '%');
-
-                 if (statusbar === 100){
-
-                   setTimeout(function(){
-                     $('.loading').remove();
-                     $('.frame').css('opacity', 1);
-                     $('.overlay').css('opacity', 0.7);
-                      setTimeout(function(){
-                        $('.intro-text').css('opacity', 1).find('.inner').addClass('moveup');
-                        $('.full-width').css({'opacity': 1, 'height':'2000px'});
-                      }, 500);
-                   }, 500);
-                 }
-
-               }
-             }, false);
-             return xhr;
-           }
-         });
+        url: url,
+        contentType: 'application/json; charset=utf-8',
+        data: {},
+        success: this.ajaxSuccess
+      });
 
 
 
 
     },
     ajaxSuccess: function(data){
+      setTimeout(function(){
+        $('.loading').remove();
+        $('.frame').css('opacity', 1);
+        $('.overlay').css('opacity', 0.7);
+        setTimeout(function(){
+          $('.intro-text').css('opacity', 1).find('.inner').addClass('moveup');
+          $('.full-width').css({'opacity': 1, 'height':'2000px'});
+        }, 200);
+      }, 100);
       for (var key in data) {
         if (data.hasOwnProperty(key)) {
 
@@ -77,33 +46,9 @@ define([], function(){
         }
       }
 
-    },
-
-    setHeights: function(){
-      var squareH = $('.square').innerWidth();
-
-      $('.square, .rectangle').each(function() {
-        $(this).css('height', squareH);
-      });
-
-      $('.square-small').each(function() {
-        $(this).css('height', squareH/2);
-      });
-
-
-
-
-
-      // get the width of a square.. that's the basis of height
-      // height of rectangles same.. height of small squares is half
-      // add the heights of each row to get full height
     }
 
 
   };
 
 });
-
-
-
-
